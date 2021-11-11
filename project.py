025d87f543ce393c6e7eb0d8f7fb034074601f43
lamyuewei <lamyuewei1997@hotmail.com>
2021-11-11 13:24:16 +0800

    Project population

--- project.py
+++ project.py
@@ -7,5 +7,9 @@ import pandas as pd
 ProjectData = pd.read_json("data/project.json")
 #print(ProjectData.head())
 
+# class Project(Resource):
+#     def get(self, id):
+#         result = Project
+
 if __name__ == "__main__":
     app.app.run(debug=True)
