1444dc0e3ba0aa6a12fc1ba5f77a3664c4b8ed99
lamyuewei <lamyuewei1997@hotmail.com>
2021-11-11 11:11:47 +0800

    add import and create database

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 from flask import Flask
+from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
+from flask_sqlalchemy import SQLAlchemy     # For Database
+
 app = Flask(__name__)
+api = Api(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+db = SQLAlchemy(app)
+#db.create_all()   ## Only do this once
 
 @app.route("/")
 
@@ -13,3 +20,4 @@ def auth(name, pw):
         return "Authenticated"
     else:
         return "Wrong password"
+
