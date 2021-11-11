7eeb990b91d5039bd1aeb01c111057569309eaa3
outbridge <outbridge1@gmail.com>
2021-11-11 12:48:48 +0800

    setting up routes and forms

--- app.py
+++ app.py
@@ -2,18 +2,50 @@ from flask import Flask, jsonify, request
 from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy     # For Database
 
+from flask_bcrypt import Bcrypt
+from flask_migrate import Migrate
+
+from flask_login import (
+    UserMixin,
+    login_user,
+    LoginManager,
+    current_user,
+    logout_user,
+    login_required,
+)
+
+login_manager = LoginManager()
+login_manager.session_protection = "strong"
+login_manager.login_view = "login"
+login_manager.login_message_category = "info"
+
+
+migrate = Migrate()
+bcrypt = Bcrypt()
+
+
 app = Flask(__name__)
 api = Api(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 db = SQLAlchemy(app)
-#db.create_all()   ## Only do this once
+
+app.secret_key = 'secret-key'
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+login_manager.init_app(app)
+db.init_app(app)
+migrate.init_app(app, db)
+bcrypt.init_app(app)
+
 
 @app.route("/")
 def home():
     return "Hi"
 
 
-@app.route('/auth', methods=['POST'])
+
+@app.route('/auth_test', methods=['GET','POST'])
 def auth():
     username = request.form.get('username')
     password = request.form.get('password')
@@ -22,4 +54,4 @@ def auth():
         return 'OK', 200
     else:
         return 'Wrong password', 401
-
+    
---
+++ routes.py
@@ -0,0 +1,74 @@
+from flask import (
+    Flask,
+    render_template,
+    redirect,
+    flash,
+    url_for,
+    session
+)
+
+from datetime import timedelta
+from sqlalchemy.exc import (
+    IntegrityError,
+    DataError,
+    DatabaseError,
+    InterfaceError,
+    InvalidRequestError,
+)
+from werkzeug.routing import BuildError
+
+from flask_bcrypt import Bcrypt,generate_password_hash, check_password_hash
+
+from flask_login import (
+    UserMixin,
+    login_user,
+    LoginManager,
+    current_user,
+    logout_user,
+    login_required,
+)
+
+from app import db,login_manager,bcrypt
+from UserDBAPI import User
+from forms import login_form
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.before_request
+def session_handler():
+    session.permanent = True
+    app.permanent_session_lifetime = timedelta(minutes=1)
+
+@app.route("/", methods=("GET", "POST"), strict_slashes=False)
+def index():
+    return render_template("index.html",title="Home")
+
+
+@app.route("/auth/", methods=("GET", "POST"), strict_slashes=False)
+def login():
+    form = login_form()
+
+    if form.validate_on_submit():
+        try:
+            user = User.query.filter_by(email=form.email.data).first()
+            if check_password_hash(user.pwd, form.pwd.data):
+                login_user(user)
+                return redirect(url_for('index'))
+            else:
+                flash("Invalid Username or password!", "danger")
+        except Exception as e:
+            flash(e, "danger")
+
+    return render_template("auth.html",
+        form=form,
+        text="Login",
+        title="Login",
+        btn_action="Login"
+        )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
